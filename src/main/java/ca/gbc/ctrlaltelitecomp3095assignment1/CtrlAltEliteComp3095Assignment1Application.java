e05d6c5418e817f5a7faaacafcc440e70a302ae4
MondiKoci <mondi.s.koci@gmail.com>
2021-10-15 16:56:27 -0400

    Springboot project initialized.

---
+++ src/main/java/ca/gbc/ctrlaltelitecomp3095assignment1/CtrlAltEliteComp3095Assignment1Application.java
@@ -0,0 +1,13 @@
+package ca.gbc.ctrlaltelitecomp3095assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtrlAltEliteComp3095Assignment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtrlAltEliteComp3095Assignment1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ca/gbc/ctrlaltelitecomp3095assignment1/CtrlAltEliteComp3095Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.ctrlaltelitecomp3095assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtrlAltEliteComp3095Assignment1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
