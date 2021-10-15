a8723b515a027f38cbb5a6ec00e70414da1d2304
JungGarY <garfeijung@gmail.com>
2021-10-15 17:12:04 -0400

    testing git connection

--- src/main/java/ca/gbc/ctrlaltelitecomp3095assignment1/CtrlAltEliteComp3095Assignment1Application.java
+++ src/main/java/ca/gbc/ctrlaltelitecomp3095assignment1/CtrlAltEliteComp3095Assignment1Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CtrlAltEliteComp3095Assignment1Application {
+    //test
 
     public static void main(String[] args) {
         SpringApplication.run(CtrlAltEliteComp3095Assignment1Application.class, args);
